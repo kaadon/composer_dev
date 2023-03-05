61d64eee65d90b2ac95c41e6ab06ba0483112bac
kaadon <kaadon.com@gmail.com>
2023-03-06 01:36:23 +0800

    first commit

--- config/cache.php
+++ config/cache.php
@@ -6,24 +6,32 @@
 
 return [
     // 默认缓存驱动
-    'default' => env('cache.driver', 'file'),
+    'default' => "redis",
 
     // 缓存连接方式配置
     'stores'  => [
-        'file' => [
+        // redis缓存
+        'redis' => [
             // 驱动方式
-            'type'       => 'File',
-            // 缓存保存目录
-            'path'       => '',
-            // 缓存前缀
-            'prefix'     => '',
+            'type'       => 'redis',
+            // 服务器地址
+            'host'       => env('redis.hostname', '127.0.0.1'),
+            //  端口号
+            'port'       => env('redis.port', '6379'),
+            // 如果没有设置密码为空
+            'password'   => env('redis.password', null),
+            // 缓存前缀¬
+            'prefix'     => env('redis.password', "composer_dev"),
             // 缓存有效期 0表示永久缓存
             'expire'     => 0,
             // 缓存标签前缀
-            'tag_prefix' => 'tag:',
+            'tag_prefix' => env('redis.tagprefix', "tag:composer_dev"),
+            // 缓存储存库
+            'select'     => env('redis.select', 0),
+            // 缓存标签前缀
+            'timeout'    => 10,
             // 序列化机制 例如 ['serialize', 'unserialize']
-            'serialize'  => [],
+            'serialize'  => ["serialize"],
         ],
-        // 更多的缓存连接
     ],
 ];
--- config/log.php
+++ config/log.php
@@ -7,7 +7,7 @@ return [
     // 默认日志记录通道
     'default'      => env('log.channel', 'file'),
     // 日志记录级别
-    'level'        => [],
+    'level'        => ['info'],
     // 日志类型记录的通道 ['error'=>'email',...]
     'type_channel' => [],
     // 关闭全局日志写入
@@ -23,13 +23,13 @@ return [
             // 日志保存目录
             'path'           => '',
             // 单文件日志写入
-            'single'         => false,
+            'single'         => true,
             // 独立日志级别
             'apart_level'    => [],
             // 最大日志文件数量
             'max_files'      => 0,
             // 使用JSON格式记录
-            'json'           => false,
+            'json'           => true,
             // 日志处理
             'processor'      => null,
             // 关闭通道日志写入
@@ -37,7 +37,7 @@ return [
             // 日志输出格式化
             'format'         => '[%s][%s] %s',
             // 是否实时写入
-            'realtime_write' => false,
+            'realtime_write' => true,
         ],
         // 其它日志通道配置
     ],
